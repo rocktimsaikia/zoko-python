e5e56babc5c0b7218317da198edab5aa8c45a77c
Rocktim Saikia <rocktimthedev@gmail.com>
2024-01-13 20:54:32 +0530

    refactor: update message sending payload and args

--- examples/send_message.py
+++ examples/send_message.py
@@ -14,11 +14,9 @@ def main():
     zoko = ZokoClient(api_key=ZOKO_API_KEY)
     payload = {
         "message": "hello world",
-        "recipient": "1234567890",
-        "template_id": "greeting_01",
-        "template_args": ["John", "Doe"],
-        "template_language": "en",
-        "template_type": "buttonTemplate",
+        "recipient": "1234567897",
+        "template_id": "_zoko_template_id_",
+        "template_type": "template",
     }
     response = zoko.message.send_message(**payload)
     print(response)
