272243666926440a3c0c4db086556073581a8c8a
rocktimsaikia <rocktimthedev@gmail.com>
2026-07-19 11:49:49 +0530

    docs: use MessageType enum in send_message example

--- examples/send_message.py
+++ examples/send_message.py
@@ -1,9 +1,9 @@
-# Get a template by passing the Zoko template ID
+# Send a template message
 import os
 
 from dotenv import load_dotenv
 
-from zoko import ZokoClient
+from zoko import MessageType, ZokoClient
 
 load_dotenv()
 
@@ -12,13 +12,12 @@ ZOKO_API_KEY = os.environ["ZOKO_API_KEY"]
 
 def main():
     zoko = ZokoClient(api_key=ZOKO_API_KEY)
-    payload = {
-        "message": "hello world",
-        "recipient": "1234567897",
-        "template_id": "_zoko_template_id_",
-        "template_type": "template",
-    }
-    response = zoko.message.send_message(**payload)
+    response = zoko.message.send_message(
+        message="hello world",
+        recipient="1234567897",
+        template_id="_zoko_template_id_",
+        template_type=MessageType.TEMPLATE,
+    )
     print(response)
 
 
